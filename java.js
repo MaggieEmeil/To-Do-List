131d1d55d86b2d853093c0fa099ece04ea9be321
MaggieEmeil <mangoooo2013@gmail.com>
2024-08-27 14:29:42 +0300

    add files

--- java.js
+++ java.js
@@ -1,99 +1,72 @@
-// let task=document.getElementById('task');
-// let addBtn=document.getElementById('addBtn');
-// let read=document.getElementById('read');
-// let mood='add';
-// let x;
-
-// // add task
-
-// let addTask;
-// if (localStorage.allTasks != null){
-//    addTask=JSON.parse (localStorage.allTasks);
-// }
-// else{
-//  addTask=[];
-// }
-
-// addBtn.onclick=function(){
-//     let newTask=task.value;
-//     if(mood === 'ADD'){
-//     addTask.push(newTask);
-//     }
-//     else{
-//       addTask[x]=newTask;
-//       mood='ADD';
-//       addBtn.innerHTML='ADD';
-//     }
-//     localStorage.setItem('allTasks'  , JSON.stringify(addTask));
-//     clearData();
-//     showData();
-//     console.log(addTask);
-// }
-
-
-// // clear data after click addBtn
-
-// function clearData(){
-//     task.value='';
-// }
-
-
-
-// // read data
-
-// function showData(){
-//     let read='';
-//     for(let i=0 ; i<addTask.length; i++){
-//       read+=`<li>${addTask[i]}</li> 
-//         <button onclick="doneBtn(${i})" type="submit" id="doneBtn">done</button>
-//         <button onclick="editBtn(${i})"  type="submit" id="edit">edit</button>
-//         <button onclick="delBtn(${i})" type="submit" id="delete">delete</button>`;
-//       console.log(read); 
-//     }
-//     document.getElementById('read').innerHTML=read;
-//     document.getElementsByClassName('item').innerHTML=item;
-// }
-// showData();
-
-
-// // delete on item
-
-// function delBtn(i){
-//       addTask.splice(i,1);
-//       localStorage.allTasks=JSON.stringify(addTask);
-//       showData();
-// }
-
-
-// // update
-
-// function editBtn(i){
-//    task.value=addTask[i];
-//    addBtn.innerHTML="EDIT";
-//    mood="EDIT";
-//    x=i;
-//    editBtn(i).style.display='none';
-// }
-
-
-// // done
-
-//  function doneBtn(i) {
-// //   // الحصول على عنصر المهمة المحدد
-//  const taskElement = document.querySelectorAll('li')[i];
-
-// //   // تغيير محتوى العنصر إلى علامة صح
-
-//     taskElement.innerHTML=`✔️ ${addTask[i]}`;
-//     //  taskElement.querySelectorAll('delete').forEach(delBtn);
-//     //  taskElement.querySelectorAll('edit').forEach(editBtn);
-// //   // حفظ التغييرات في localStorage إذا لزم الأمر
-//   localStorage.setItem('allTasks', JSON.stringify(addTask));
-// }
-
-
 
 
 let task=document.getElementById('task');
-let addBtn=document.getElementById('addBtn');
+let addBtn=document.getElementById('addBtn').addEventListener("click", addTask);
 let read=document.getElementById('read');
+// let addTask;
+let result=[];
+// if (localStorage.addTask != null){
+//    result=JSON.parse (localStorage.addTask);
+// }
+// else{
+//  result=[];
+// }
+
+// create task
+
+function addTask(){
+ let arr=task.value;
+ result.push(arr);
+ displayData();
+console.log(result);
+task.value="";
+}
+
+    // localStorage.setItem('addTask'  , JSON.stringify(result));
+// display task
+function displayData(){
+  let show='';
+  for (let i=0; i<result.length; i++){
+    show+=`<li>${result[i]}</li> 
+            <button onclick="doneBtn(${i})" type="submit" id="doneBtn">done</button>
+            <button onclick="editBtn(${i})"  type="submit" id="editBtn">edit</button>
+            <button onclick="delBtn(${i})" type="submit" id="delbtn">delete</button>`;
+          console.log(show); 
+  } 
+   document.getElementById('read').innerHTML=show;
+}
+// delete btn
+function delBtn(i){
+
+        result.splice(i,1);
+        // localStorage.addTask=JSON.stringify(result);
+        displayData();
+}
+  delBtn()  
+// update btn
+
+function editBtn(i){
+  document.getElementById('editInput').style.display='inline';
+  document.getElementById('editButton').style.display='inline';
+  document.getElementById('editInput').value = result[i];
+ document.getElementById('editButton').onclick = ()=> editbutton(i)
+
+}
+function editbutton(i){
+  document.getElementById('editInput').style.display='none';
+  document.getElementById('editButton').style.display='none';
+  result[i] = document.getElementById('editInput').value
+  displayData();
+  // document.getElementsByTagName("li").textContent=document.getElementById('editInput').value
+}
+
+// done btn
+function doneBtn(i){
+ 
+  const taskElement = document.getElementsByTagName("li")[i];
+  taskElement.style.color="green";
+  // localStorage.setItem('addTask', JSON.stringify(result));
+
+}
+doneBtn();
+
